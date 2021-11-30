323dab0e0674d3cba8849fc05769906d81c20971
sujeet-98 <42945195+sujeet-98@users.noreply.github.com>
2021-11-30 11:45:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res) => res.send('hello world'));
+app.get('/', (req, res) => res.send('hello '));
 
 app.listen(3000, () => {
     console.log('My rest api running on port 3000!');
