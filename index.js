3cfd16793d60b4e590f1549357031c1bf175faa9
sujeet-98 <42945195+sujeet-98@users.noreply.github.com>
2021-12-22 16:23:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res) => res.send('hello sujeet build '));
+app.get('/', (req, res) => res.send('hello sujeet build trigger '));
 
 app.listen(3000, () => {
     console.log('My rest api running on port 3000!');
