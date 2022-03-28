33b7db6b7db623160deac8e4ff8a57d3f1d3d6d0
sujeet-98 <42945195+sujeet-98@users.noreply.github.com>
2022-03-28 15:22:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res) => res.send('hello test1 '));
+app.get('/', (req, res) => res.send('hello appp '));
 
 app.listen(8080, () => {
     console.log('My rest api running on port 8080!');
